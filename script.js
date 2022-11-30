9d22434e32fd5da0bf2d3a42ea415f44b306bfb0
bighen <henryk_drozd@wp.pl>
2022-11-30 06:05:00 -0500

    Labo z kursu

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+   if (principal<=0)
+        {
+        alert("Wprowadź dodatnią liczbę");
+        document.getElementById("principal").focus();
+        return;
+        }
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   var interest = principal * years * rate /100;
+   var year = new Date().getFullYear()+parseInt(years);
+
+
+   document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of \<b\>"+interest+",\</b\>\<br\>in the year "+year+"\<br\>"
+   //document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "      +rate+"%\<br\>You will receive an amount of "+interest;
+
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function updateResult()
+{
+    var resultval = document.getElementById("result");
+    var textResult = "Jeśłi wpłacisz depozyt " + document.getElementById("principal").value +
+        "<br>ze stopą procentową " + document.getElementById("rate").value +
+        "<br>otrzymasz wypłatę " + document.getElementById("interest").value +
+        "<br>w roku " + year
+        resultval.innerHTML = testResult;
+}        
